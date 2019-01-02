c57bc617a2d4917c73cfe328ab69f0b01cf7188e
Truman <truman.shuck@gmail.com>
2019-01-02 12:38:40 -0600

    Replace references to react-emotion with emotion/styled

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import styled from 'react-emotion'
+import styled from '@emotion/styled'
 
 import { widths } from '../styles/variables'
 import { getEmSize } from '../styles/mixins'
